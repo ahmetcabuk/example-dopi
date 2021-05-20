565eaacfc51b1e039d883b367ab4886c96b9637f
ahmetcabuk <ahmetcabuk07@gmail.com>
2021-05-20 12:16:20 +0300

    Added TestObject & testScript

---
+++ e-dopi/Assets/testScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    public bool test = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
