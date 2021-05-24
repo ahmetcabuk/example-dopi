e050fd24a553d6997ac776b8b9007afac3cebac7
ahmetcabuk <ahmetcabuk07@gmail.com>
2021-05-24 16:40:24 +0300

    Added Walk Animation

--- e-dopi/Assets/Script/Third Person Characther/CharacterAnimation.cs
+++ e-dopi/Assets/Script/Third Person Characther/CharacterAnimation.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class CharacterAnimation : MonoBehaviour
 {
     public VariableJoystick variableJoystick;
+    [Range(0.1f, 0.9f)]
+    public float runLimit;
 
     private Animator animator;
-    //private bool isMoving = false;
-
 
     private void Start()
     {
@@ -19,16 +19,26 @@ public class CharacterAnimation : MonoBehaviour
     {
         Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
 
-        Debug.Log(direction);
-
         if (direction != new Vector3(0,0,0))
         {
-            animator.SetBool("isRunning", true);
+            transform.eulerAngles = new Vector3(0, Mathf.Atan2(direction.x, direction.z) * 180 / Mathf.PI, 0);
+
+            animator.SetBool("isWalking", true);
+
+            if (direction.x > runLimit || direction.x < -runLimit ||  direction.z > runLimit || direction.z < -runLimit)
+            {
+                animator.SetBool("isWalking", false);
+                animator.SetBool("isRunning", true);
+            }
+            else
+            {
+                animator.SetBool("isRunning", false);
+            }
         }
         else
         {
+            animator.SetBool("isWalking", false);
             animator.SetBool("isRunning", false);
         }
-
     }
 }
